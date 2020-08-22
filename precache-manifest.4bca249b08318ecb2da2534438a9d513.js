ab069f8199ed040bbf5b746e0e23f2323c6fed8f
Shan <shan.mi.fanfan@gmail.com>
2020-08-22 11:39:45 +0200

    Updates

---
+++ precache-manifest.4bca249b08318ecb2da2534438a9d513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc034c82a06642d4178be94374309b0",
+    "url": "/Shan-Mi/marvel-api-react-task1/index.html"
+  },
+  {
+    "revision": "69d1f48996bf1d965f83",
+    "url": "/Shan-Mi/marvel-api-react-task1/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c22b69b3545f4861a695",
+    "url": "/Shan-Mi/marvel-api-react-task1/static/css/main.ae0ab4c9.chunk.css"
+  },
+  {
+    "revision": "69d1f48996bf1d965f83",
+    "url": "/Shan-Mi/marvel-api-react-task1/static/js/2.f01a39b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shan-Mi/marvel-api-react-task1/static/js/2.f01a39b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22b69b3545f4861a695",
+    "url": "/Shan-Mi/marvel-api-react-task1/static/js/main.94890e6d.chunk.js"
+  },
+  {
+    "revision": "a08d50494cb4c4e70e6c",
+    "url": "/Shan-Mi/marvel-api-react-task1/static/js/runtime-main.adf02a9d.js"
+  }
+]);
