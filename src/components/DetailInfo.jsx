3ff5e49ee73e683b019ec529d705d5ab2440c325
Shan <shan.mi.fanfan@gmail.com>
2020-08-24 11:18:38 +0200

    clear head, delete bullshit

--- src/components/DetailInfo.jsx
+++ src/components/DetailInfo.jsx
@@ -5,16 +5,13 @@ import { NameListContainer } from "./CharDetail.styles";
 import DetailInfoContainer from "./DetailInfoContainer";
 
 const DetailInfo = ({ catagory, amount, items }) => {
-  const isEmpty = !!amount;
   return (
     <div>
       <h3>{`Available ${catagory} amount: ${amount}`}</h3>
       <NameListContainer>
-        {!isEmpty ? (
-          <DetailInfoContainer items={items} />
-        ) : (
-          items.map((items, i) => <DetailInfoContainer key={i} items={items} />)
-        )}
+        {items.map((items, i) => (
+          <DetailInfoContainer key={i} items={items} />
+        ))}
       </NameListContainer>
     </div>
   );
