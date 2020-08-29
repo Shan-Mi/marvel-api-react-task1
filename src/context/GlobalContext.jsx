c907d62335e509a46e7b73dcc182d1b1ba8e6c1e
Shan <shan.mi.fanfan@gmail.com>
2020-08-29 17:29:20 +0200

    refactor, destructure props directly, cut the crap

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,9 +1,11 @@
 import React, { createContext } from "react";
 import useFetch from "../components/useFetch";
 
+// should we write like this: createContext({})??
+//maybe then we don't need to destructure data out??
 export const CharactersContext = createContext();
 
-const GlobalContext = (props) => {
+const GlobalContext = ({ children }) => {
   const [data, isLoading, attrText] = useFetch(
     "https://mock-data-api.firebaseio.com/marvel-characters.json",
     []
@@ -11,7 +13,7 @@ const GlobalContext = (props) => {
 
   return (
     <CharactersContext.Provider value={{ data, isLoading, attrText }}>
-      {props.children}
+      {children}
     </CharactersContext.Provider>
   );
 };
